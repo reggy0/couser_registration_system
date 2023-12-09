0f48b52ef529edf44cc1642d5b47200e423bd8bb
reggy0 <85451025+reggy0@users.noreply.github.com>
2023-12-09 01:25:58 -0800

    Update urls.py
    new

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+# urls.py
+
+from django.urls import path
+from .views import course_list, course_add, course_delete, course_add_student
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('courses/', include('manage.urls')),
+    path('list/', course_list, name='course_list'),
+    path('add/', course_add, name='course_add'),
+    path('add_student/<str:course_id>/', course_add_student, name='course_add_student'),
 ]
